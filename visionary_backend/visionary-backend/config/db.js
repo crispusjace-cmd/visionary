5a3bfaa2ee57406ad60b5a004dd0a3be5505a713
crisp <crispusjace@gmail.com>
2026-03-20 19:51:57 +0300

    fix db.js syntax error

--- visionary_backend/visionary-backend/config/db.js
+++ visionary_backend/visionary-backend/config/db.js
@@ -1,18 +1,18 @@
-const { Pool } + require('pg');
-require('dotenv').config();
+const { Pool } + require ('pg');
+reqiure('dotenv').config();
 
 const pool = new Pool({
- connectionString: process.env.DATABASE_URL,
- ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false} :
-false});
+  connectionString: process.evn.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? {  rejectUnauthorized: false } : false 
+});
 
 pool.connect((err, client, release) => {
-  if (err) {
-    console.error('x  Database connection  failed:', err.message);
-  }else {
-   console.log(' Connected to PostgreSQL successfully!');
-   release();
-  }
+ if (err) {
+ console.error('Database connection failed:', err.message);
+} else {
+  console.log('Connected to PostgreSQL successfully!');
+  release();
+ }
 });
 
 module.exports = pool;
